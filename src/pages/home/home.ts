799e6110e20a09174705f50d071f3134b8ebf297
youpiyoful <youpiyoful@Host-002.lan>
2018-11-14 01:09:48 +0100

    :construction: 2 new pages add : books and single-book

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BooksPage } from '../books/books';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,9 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+  onGoToBooks() {
+    this.navCtrl.push(BooksPage);
   }
 
 }
